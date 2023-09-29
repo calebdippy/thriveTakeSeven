ba721000901e445cb6165c172a18797336f86849
calebdippy <141693653+calebdippy@users.noreply.github.com>
2023-09-29 19:50:46 +1300

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,28 @@
+var i=0; //Start point
+var images=[];
+var time=3000;
+
+//Image List
+ images[0] = "mountain-background.jpg";
+ images[1] = "tractor-background.jpg";
+ images[2] = "harvister-background-image.jpg";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
